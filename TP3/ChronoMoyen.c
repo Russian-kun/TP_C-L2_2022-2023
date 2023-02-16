6af4b589c8b11ea34f4b8aff693f38791636405e
Russian-kun <axel.baillargeau@gmail.com>
2023-02-16 10:45:24 +0100

    fini chrono

--- TP3/ChronoMoyen.c
+++ TP3/ChronoMoyen.c
@@ -4,36 +4,56 @@ int main() {
     initscr();
     nodelay(stdscr, TRUE);
     keypad(stdscr, TRUE);
+    noecho();
 
     struct timeval *deb = malloc(sizeof(struct timeval));
     struct timeval *fin = malloc(sizeof(struct timeval));
 
     gettimeofday(deb, NULL);
+    int delai = 100000;
 
     long int duree_totale = 0;
-    bool chrono = 0;
+    bool chrono = FALSE;
+    // char *timer[4] = {"00",
+    //                   "00",
+    //                   "00",
+    //                   "00"};
 
     int touche = -1;
 
     while (1) {
-        gettimeofday(fin, NULL);
         touche = getch();
 
         if (touche == 32) {  // Code de la touche espace
-            chrono = 0 ? 1 : 0;
+            if (chrono == FALSE) {
+                chrono = TRUE;
+            } else {
+                chrono = FALSE;
+            }
             if (chrono) {
                 gettimeofday(deb, NULL);
-                int tmp = (int)(duree_totale / 1000);
-                deb->tv_sec -= tmp;
-                deb->tv_usec -= (int)(duree_totale - tmp);
+                int sec_tmp = (int)(duree_totale / 1000);
+                deb->tv_sec -= sec_tmp;
+                deb->tv_usec -= (duree_totale - sec_tmp * 1000) * 1000;
+            } else {
+                mvprintw(0, 0, "   ");
             }
-        }
+        } else if (touche == KEY_BACKSPACE)
+            break;
+
         if (chrono) {
+            gettimeofday(fin, NULL);
             duree_totale = intervalle_ms(*deb, *fin);
         }
 
-        mvprintw(0, 0, "%ld", duree_totale);
+        mvprintw(LINES / 2, COLS / 2, "%ld", duree_totale);
+        refresh();
+        usleep(delai);
     }
 
+    free(deb);
+    free(fin);
+    clear();
+    endwin();
     return 0;
 }
