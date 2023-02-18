01417786ccff71b27d623f194f0ebd35c6d30679
Russian-kun <axel.baillargeau@gmail.com>
2023-02-19 00:57:04 +0100

    couleur

--- TP3/Chrono.c
+++ TP3/Chrono.c
@@ -3,6 +3,10 @@
 
 int main() {
     initscr();
+    start_color();
+    init_pair(1, COLOR_RED, COLOR_BLACK);
+    init_pair(2, COLOR_BLUE, COLOR_BLACK);
+
     keypad(stdscr, TRUE);
     noecho();
     curs_set(0);
