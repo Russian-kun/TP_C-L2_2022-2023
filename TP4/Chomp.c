1fb9fa268c486c8809c7e75d12d072d154ee02dc
Russian-kun <axel.baillargeau@gmail.com>
2023-02-24 16:04:52 +0100

    main loop

--- TP4/Chomp.c
+++ TP4/Chomp.c
@@ -6,9 +6,11 @@ int main() {
     initscr();
     curs_set(0);
     noecho();
+    keypad(stdscr, TRUE);
+    mousemask(ALL_MOUSE_EVENTS, NULL);
 
     start_color();
-    init_pair(1, COLOR_BLACK, COLOR_BLACK);
+    init_pair(1, COLOR_RED, COLOR_BLACK);
 
     Position pos;
     pos.carre = creer_tablette();
@@ -16,18 +18,21 @@ int main() {
     Joueur gagnant;
 
     while (pos.carre.tab[0][0]) {
+        mvprintw(0, 0, "Tour du joueur %d", pos.player + 1);
         afficher_position(pos);
+        // mvprintw(LINES / 2, COLS / 2, "O");
         refresh();
 
         coup = lire_coup(pos);
         jouer_coup(&pos, coup);
         if (est_jeu_termine(pos, &gagnant))
             break;
+        clear();
     }
     clear();
 
     int len = strlen("Victoire du Joueur  ");
-    mvprintw(LINES / 2, COLS / 2 - len / 2, "Victoire du Joueur %d", gagnant);
+    mvprintw(LINES / 2, COLS / 2 - len / 2, "Victoire du Joueur %d", gagnant + 1);
 
     refresh();
     getch();
