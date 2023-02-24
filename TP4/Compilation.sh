8740aa8733d8a00a0d640b306335dedc831fc315
Russian-kun <axel.baillargeau@gmail.com>
2023-02-24 16:04:22 +0100

    script shell

--- TP4/Compilation.sh
+++ TP4/Compilation.sh
@@ -1,3 +1,4 @@
 gcc -c common.c common.h -Wall -Wfatal-errors -fdiagnostics-color=always -lncurses
 gcc -c common.o graphique.c graphique.h -Wall -Wfatal-errors -fdiagnostics-color=always -lncurses
 gcc Chomp.c common.o graphique.o -Wall -Wfatal-errors -fdiagnostics-color=always -lncurses -o Chomp
+gcc Chomp_non_modulaire.c -Wall -Wfatal-errors -fdiagnostics-color=always -lncurses -o Chomp_non_modulaire
