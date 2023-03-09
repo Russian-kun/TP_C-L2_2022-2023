71c99c9161eda6a4bc8e11490c5fa65f8887d327
Russian-kun <axel.baillargeau@gmail.com>
2023-03-09 01:49:14 +0100

    main

---
+++ TP5/Snake.c
@@ -0,0 +1,59 @@
+#include "logique.h"
+#include "graphisme.h"
+#include <time.h>
+#include <unistd.h>
+
+#define SNAKE_MAX_LENGTH 100
+#define NB_LIGNES 20
+#define NB_COLONNES 20
+#define TAILLE_SNAKE 5
+
+int main() {
+    // Initialisation de ncurses
+    initscr();
+    cbreak();
+    noecho();
+    keypad(stdscr, TRUE);
+    curs_set(0);
+    nodelay(stdscr, TRUE);
+    srand(time(NULL));
+
+    // Initialisation du monde
+    Monde mon = monde_initialiser(NB_LIGNES, NB_COLONNES, TAILLE_SNAKE);
+
+    // Affichage initial
+    interface_afficher_monde(mon);
+
+    refresh();
+
+    // Boucle principale
+    while (!monde_est_mort_serpent(mon)) {
+        // On attend un peu
+        usleep(250000);
+
+        // On récupère les entrées clavier
+        interface_piloter(&mon);
+
+        // On met à jour le monde
+        monde_maj(&mon);
+
+        // On affiche le monde
+        interface_afficher_monde(mon);
+
+        refresh();
+    }
+    afficher_victoire(mon);
+    refresh();
+
+    // On attend que l'utilisateur appuie sur une touche
+    nodelay(stdscr, FALSE);
+    getch();
+
+    // On quitte ncurses
+    endwin();
+
+    // On libère la mémoire
+    free_monde(mon);
+
+    return 0;
+}
